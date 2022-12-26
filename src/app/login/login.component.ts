a1ad67a0c5915a1f5993d2e0598ff754440916d0
Surya Baskar <suryabaskar139@gmail.com>
2022-12-26 06:26:28 -0800

    source

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  //Form variables
+registerForm:any = FormGroup;
+submitted = false;
+constructor( private formBuilder: FormBuilder){}
+//Add user form actions
+get f() { return this.registerForm.controls; }
+onSubmit() {
+  
+  this.submitted = true;
+  // stop here if form is invalid
+  if (this.registerForm.invalid) {
+      return;
+  }
+  //True if all the fields are filled
+  if(this.submitted)
+  {
+    alert("Great!!");
+  }
+ 
+}
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+    });
+  }
 }
