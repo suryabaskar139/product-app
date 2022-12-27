39f0c3f0fbdcfacab28311eaf93d6e167fe26cc9
Surya Baskar <suryabaskar139@gmail.com>
2022-12-27 03:30:36 -0800

    source

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './Login/account/account.component';
+import { EmployeeComponent } from './Login/employee/employee.component';
+import { PhoneComponent } from './Login/phone/phone.component';
 
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'Account',component:AccountComponent}
+  {path:'Account',component:AccountComponent},
+  {path:'Employee',component:EmployeeComponent},
+  {path:'Phone',component:PhoneComponent}
 
 ];
 
