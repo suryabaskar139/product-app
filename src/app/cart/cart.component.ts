5a5746fe9019c8e770fb9ef54dea234fd9123c24
Surya Baskar <suryabaskar139@gmail.com>
2023-03-10 20:20:04 +0530

    src

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+  inputnumber = 0;
+  
+  plus()
+  {
+   this.inputnumber = this.inputnumber+1;
+  }
+  minus()
+  {
+    if(this.inputnumber != 0)
+  {
+   this.inputnumber = this.inputnumber-1;
+  }
+  
+  }
 }
