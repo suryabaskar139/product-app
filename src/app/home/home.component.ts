1d6242652467ff8a9140740f9c2c9788e29c5851
Surya Baskar <suryabaskar139@gmail.com>
2023-03-13 15:32:54 +0530

    src

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  homeOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<img src="../../assets/images/Group-49014.svg">', '<img src="../../assets/images/Group-49015.svg">'],
+    responsive: {
+      0: {
+        items: 2
+      },
+      400: {
+        items:2
+      },
+      740: {
+        items:3
+      },
+      940: {
+        items:3
+      },
+      1140:{
+        items:4
+      },
+      1300:{
+        items:4
+      }
+      
+    },
+    nav: true
+  }
 }
